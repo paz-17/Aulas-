664b8aa191de0879d3d154cf7ed5f756fd84addf
maria clara paz <mclara04153@gmail.com>
2025-03-31 14:56:58 -0300

    while comando regresivo

---
+++ while/while06.py
@@ -0,0 +1,9 @@
+while True:
+      numero = int(input("Digite um número possitivo: "))
+
+      if numero < 0:
+          print("o número deve ser possitivo")
+          continue
+      else:
+          print("parabens o número é possitivo")
+          break
