11a8c0aa5ec52652140f2732b523d22b6aa873e0
maria clara paz <mclara04153@gmail.com>
2025-03-31 13:25:02 -0300

    cria exercicio decremento while.

---
+++ while/while01.py
@@ -0,0 +1,6 @@
+#atividade 1
+
+contador = 1
+while  contador <= 5:
+    print(contador)
+    contador = contador +1
---
+++ while/while02.py
@@ -0,0 +1,5 @@
+#atividade
+x = 10
+while x > 0:
+    print("loop infinito! ")
+    x = x -1
