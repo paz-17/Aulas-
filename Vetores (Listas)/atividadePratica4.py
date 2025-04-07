95483ead898ba8160cb19620d693b69672c77946
maria clara paz <mclara04153@gmail.com>
2025-04-07 10:36:40 -0300

    atividades

---
+++ Vetores (Listas)/atividadePratica4.py
@@ -0,0 +1,5 @@
+filmes = ["culpa mia", "it a coisa", "sobre as aguas do sena", "barraca do beijo", "atraves da minha janela","as branquelas","a culpa e das estrelhas"]
+
+print('Filmes em minha lista')
+for  filmes in filmes:
+    print(filmes)
---
+++ Vetores (Listas)/exemplo4.py
@@ -0,0 +1,9 @@
+#Criando uma lista de números
+numeros = [5, 10,15, 20, 25, 30]
+
+#Descobrindo o tamanho da lista
+tamanho_da_lista = len(numeros)
+
+#exibindo o resultado
+print(f"O Tamanho da lista é: {tamanho_da_lista}")
+#saida: o tamanho da lista é: 6+
