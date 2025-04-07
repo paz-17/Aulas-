6e3a9585ac0bb3ba2539dcda0e46bbae2583770c
maria clara paz <mclara04153@gmail.com>
2025-04-07 08:42:07 -0300

    cria elementos de uma lista

--- Vetores (Listas)/elementosDeUmaLista.py
+++ Vetores (Listas)/elementosDeUmaLista.py
@@ -3,3 +3,8 @@ numeros = [10,20,30,40,50]
 #Acessando o primeiro número (índice 0)
 primeiro = numeros [0] #10
 
+#Acessando o terceiro número (índice 2)
+primeiro = numeros [2] #30
+
+#Acessando o último número (índice -1)
+ultimo = numeros[-1] #50
