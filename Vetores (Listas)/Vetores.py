e385483117c5111246193a3a89c8dc9f9c9b7b3e
maria clara paz <mclara04153@gmail.com>
2025-04-07 07:55:11 -0300

    cria lista vetores

---
+++ Vetores (Listas)/Vetores.py
@@ -0,0 +1,6 @@
+# Isso é uma lista de compras
+
+lista_de_comprinhas = ["gelatina capilar", "toca de cetim", "calça jens","base de unha"]
+
+# A função 'print' exibe o conteúdo da lista na tela
+print(lista_de_comprinhas)
---
+++ while/while07.py
@@ -0,0 +1,3 @@
+while True:
+    operaçao = int(input("digite uma operação: "))
+    numero2 = int(input("digite o 2 número: "))
