1521b23d5de78506f32a2892742c68e9b271d8be
maria clara paz <mclara04153@gmail.com>
2025-04-07 09:58:27 -0300

    exemplo

--- Vetores (Listas)/exemplo2.py
+++ Vetores (Listas)/exemplo2.py
@@ -1,6 +1,6 @@
 materialEscolar = ['lapís', 'estojo', 'mochila', 'bolsinha', 'borracha', 'garrafa']
 
 materialEscolar.remove('bolsinha')
-materialEscolar.pop(3)
+#materialEscolar.pop(3)
 
 print(materialEscolar)
