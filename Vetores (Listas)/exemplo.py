b517e0fb35269f83c28d3d055f16d215a0dbd872
maria clara paz <mclara04153@gmail.com>
2025-04-07 09:42:34 -0300

    exemplo

---
+++ Vetores (Listas)/exemplo.py
@@ -0,0 +1,15 @@
+#exemplo1
+animais = []
+
+animais.append('Cachorro')
+print(animais)
+
+#exemplo2
+animais = ["cachorro", "gato"]
+animais.append("pássaro")
+#Resulatado: ["cachorro", "gato","passaro"]
+
+#exemplo3
+animais = ["cachorro", "gato"]
+animais.append("coelho")
+#Resulatado: ["cachorro", "gato","coelho"]
