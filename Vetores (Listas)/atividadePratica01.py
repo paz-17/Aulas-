d158fb5ebaa6fa253bc837b8866c2c071e72795e
maria clara paz <mclara04153@gmail.com>
2025-04-07 08:52:56 -0300

    atividade pratica01

---
+++ Vetores (Listas)/atividadePratica01.py
@@ -0,0 +1,9 @@
+cidades= ["São Paulo", "Avaré", "Rio De Janeiro", "Arandu"]
+print(cidades)
+
+segundo = cidades[1] #Avaré
+print(segundo)
+
+ultimo = cidades [-1] #Arandu
+print(ultimo)
+
--- Vetores (Listas)/elementosDeUmaLista.py
+++ Vetores (Listas)/elementosDeUmaLista.py
@@ -2,9 +2,10 @@ numeros = [10,20,30,40,50]
 
 #Acessando o primeiro número (índice 0)
 primeiro = numeros [0] #10
-
+print(primeiro)
 #Acessando o terceiro número (índice 2)
-primeiro = numeros [2] #30
-
+terceiro = numeros [2] #30
+print(terceiro)
 #Acessando o último número (índice -1)
 ultimo = numeros[-1] #50
+print(ultimo)
