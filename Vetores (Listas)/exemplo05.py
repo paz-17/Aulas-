082ad153a73719ba379989b87c857d127645d4a2
maria clara paz <mclara04153@gmail.com>
2025-04-14 14:01:03 -0300

    Adiciona commit inicial

---
+++ Vetores (Listas)/exemplo05.py
@@ -0,0 +1,8 @@
+numeros = [3,1,4,2]
+numeros.sort()
+print(numeros) # [1,2,3,4]
+
+
+numeros = ["maria","rafa","manu"]
+nomes.sort()
+print(nomes) # ["maria","rafa","manu"]
---
+++ Vetores (Listas)/exemplo6.py
@@ -0,0 +1,7 @@
+numeros = [3,1,4,2]
+numeros.sort(reverse=True)
+print(numeros) # [1,2,3,4]
+
+numeros = ["maria","rafa","manu"]
+nomes.sort(reverse=True)
+print(nomes) # ["maria","rafa","manu"]
